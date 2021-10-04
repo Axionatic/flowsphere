b55eb11e6dc4ce81c727ff72c2d1a63cf0c31593
Axionatic <dingwall.mark@gmail.com>
2021-10-05 00:27:11 +1100

    messing with the circle, trying to make a blob. Not quite there yet.

--- src/app/flowsphere.js
+++ src/app/flowsphere.js
@@ -3,27 +3,40 @@ import p5 from "p5";
 
 const simplex = new SimplexNoise();
 const p5Container = document.getElementById('p5_container');
+const FRAMERATE = 60;
+const LOOP_FRAMES = 180;
+const BLOB_VERTICES = 16;
+const BASE_RADIUS = 100;
+const NOISE_SPEED = 0.5;
+const NOISE_INF = 0.5;
+const NOISE_STEP = 0.2;
 
-const rX = Math.round(Math.random() * 1000);
-const rY = Math.round(Math.random() * 1000);
+var rotate = Math.PI * 2 / BLOB_VERTICES;
 
 const sketch = (p) => {
   p.setup = function () {
-    p.frameRate(60);
+    p.frameRate(FRAMERATE);
     p.createCanvas(500, 500);
-    p.ellipseMode(p.RADIUS);
+    p.noStroke();
     p.fill(255);
   };
 
   p.draw = function () {
     p.background(0);
-    let n = (p.frameCount % 300) / 300 * p.TWO_PI;
-    p.ellipse(
-      p.width / 2,
-      p.height / 2,
-      100 * (simplex.noise2D(Math.sin(n) * 0.4, rX) + 1),
-      100 * (simplex.noise2D(Math.cos(n) * 0.4, rY) + 1)
-    );
+    // current frame in loop
+    let f = (p.frameCount % LOOP_FRAMES) / LOOP_FRAMES * p.TWO_PI;
+    p.push();
+    p.translate(p.width / 2, p.height / 2);
+    for (let i = 0; i < BLOB_VERTICES; i++) {
+      // generate noise for current vertex
+      let noise = simplex.noise2D((Math.sin(i * NOISE_STEP)), Math.cos(f) * NOISE_SPEED);
+      noise = p.map(noise, -1, 1, 1 - NOISE_INF, 1 + NOISE_INF);
+      // apply noise and draw
+      p.rotate(rotate);
+      p.fill(255 * (i * rotate / p.TWO_PI), 128, 128);
+      p.ellipse(BASE_RADIUS * noise, 0, 10, 10);
+    }
+    p.pop();
   };
 }
 
