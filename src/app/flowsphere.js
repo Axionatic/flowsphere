88317c427b3b53e1df6f391f8251cec2b05b87f3
Axionatic <dingwall.mark@gmail.com>
2021-10-05 21:54:15 +1100

    made blob more blobby

--- src/app/flowsphere.js
+++ src/app/flowsphere.js
@@ -4,40 +4,56 @@ import p5 from "p5";
 const simplex = new SimplexNoise();
 const p5Container = document.getElementById('p5_container');
 const FRAMERATE = 60;
-const LOOP_FRAMES = 180;
-const BLOB_VERTICES = 16;
-const BASE_RADIUS = 100;
-const NOISE_SPEED = 0.5;
-const NOISE_INF = 0.5;
-const NOISE_STEP = 0.2;
+const BLOB_VERTICES = 50;
+const BASE_RADIUS = 200;
+const LOOP_SECONDS = 10;
+const NOISE_INFLUENCE = 0.4;
 
-var rotate = Math.PI * 2 / BLOB_VERTICES;
+let loopFrames = FRAMERATE * LOOP_SECONDS;
+let rotate = Math.PI * 2 / BLOB_VERTICES;
 
 const sketch = (p) => {
   p.setup = function () {
     p.frameRate(FRAMERATE);
-    p.createCanvas(500, 500);
+    p.createCanvas(800, 800);
     p.noStroke();
     p.fill(255);
   };
 
   p.draw = function () {
     p.background(0);
-    // current frame in loop
-    let f = (p.frameCount % LOOP_FRAMES) / LOOP_FRAMES * p.TWO_PI;
+    // prepare to draw. Derive noise input from framecount
+    let n = (p.frameCount % loopFrames) / loopFrames * p.TWO_PI;
     p.push();
     p.translate(p.width / 2, p.height / 2);
     for (let i = 0; i < BLOB_VERTICES; i++) {
-      // generate noise for current vertex
-      let noise = simplex.noise2D((Math.sin(i * NOISE_STEP)), Math.cos(f) * NOISE_SPEED);
-      noise = p.map(noise, -1, 1, 1 - NOISE_INF, 1 + NOISE_INF);
-      // apply noise and draw
+      // offset vertex noise calc by position in blob
+      let vertexStep = i / BLOB_VERTICES * p.TWO_PI;
+      // generate noise and scale to act as multiplier for base radius
+      let noise = simplex.noise2D(Math.sin(n - vertexStep), Math.cos(n - vertexStep));
+      noise = p.map(noise, -1, 1, 1 - NOISE_INFLUENCE, 1 + NOISE_INFLUENCE);
+      // draw
       p.rotate(rotate);
       p.fill(255 * (i * rotate / p.TWO_PI), 128, 128);
       p.ellipse(BASE_RADIUS * noise, 0, 10, 10);
     }
     p.pop();
   };
+
+  /**
+   * sort-of sin & cos, but all straight lines instead of curves. Also with a period of 4 (so sin(x * PI/2))
+   * plotted on a graph, the output would have the same apogees/nadirs, but be diagonal
+   * @param {number} frameCount the sketch's current frame count
+   * @returns {[number, number]} [sin-ish, cos-ish] of given number
+   */
+  const diagonalTrigs = (frameCount) => {
+    let s = (frameCount + 3) % 4;
+    let c = frameCount % 4;
+    return [
+      Math.abs(2 - s) - 1,
+      Math.abs(2 - c) - 1
+    ];
+  }
 }
 
 export const flowsphere = () => { new p5(sketch, p5Container); }
